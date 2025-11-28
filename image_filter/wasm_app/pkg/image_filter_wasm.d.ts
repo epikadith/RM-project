7380d9b1ff1808958e6d19ac8de03ba1bce24267
Davan <davanhs470@gmail.com>
2025-11-28 08:14:24 +0530

    Refactor WebAssembly bindings and update type definitions for improved functionality and clarity

--- image_filter/wasm_app/pkg/image_filter_wasm.d.ts
+++ image_filter/wasm_app/pkg/image_filter_wasm.d.ts
@@ -1,41 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-export function multiply_matrices_dynamic(data_a: Float64Array, data_b: Float64Array, n: number): Float64Array;
-export function grayscale_image(data: Uint8Array): Uint8Array;
-export function sort_array(data: Int32Array): Int32Array;
 export function k_means_clustering(points: Float64Array, k: number, iterations: number): Float64Array;
-
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
-
-export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly multiply_matrices_dynamic: (a: number, b: number, c: number, d: number, e: number) => [number, number];
-  readonly grayscale_image: (a: number, b: number) => [number, number];
-  readonly sort_array: (a: number, b: number) => [number, number];
-  readonly k_means_clustering: (a: number, b: number, c: number, d: number) => [number, number];
-  readonly __wbindgen_externrefs: WebAssembly.Table;
-  readonly __wbindgen_malloc: (a: number, b: number) => number;
-  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_start: () => void;
-}
-
-export type SyncInitInput = BufferSource | WebAssembly.Module;
-/**
-* Instantiates the given `module`, which can either be bytes or
-* a precompiled `WebAssembly.Module`.
-*
-* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
-*
-* @returns {InitOutput}
-*/
-export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
-
-/**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
-*
-* @returns {Promise<InitOutput>}
-*/
-export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
+export function multiply_matrices_dynamic(data_a: Float64Array, data_b: Float64Array, n: number): Float64Array;
+export function sort_array(data: Int32Array): Int32Array;
+export function grayscale_image(data: Uint8Array): Uint8Array;
--- image_filter/wasm_app/pkg/image_filter_wasm_bg.wasm.d.ts
+++ image_filter/wasm_app/pkg/image_filter_wasm_bg.wasm.d.ts
@@ -1,10 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const multiply_matrices_dynamic: (a: number, b: number, c: number, d: number, e: number) => [number, number];
 export const grayscale_image: (a: number, b: number) => [number, number];
-export const sort_array: (a: number, b: number) => [number, number];
 export const k_means_clustering: (a: number, b: number, c: number, d: number) => [number, number];
+export const multiply_matrices_dynamic: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const sort_array: (a: number, b: number) => [number, number];
 export const __wbindgen_externrefs: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_free: (a: number, b: number, c: number) => void;
